80a6ab426dfb389d25132e33b39b26c73a70bfab
Frank <frankinbox@gmail.com>
2022-10-26 19:15:50 -0500

    update program adress and assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,53 +2,40 @@ import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import type { NextPage } from "next";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import {
+  useProgram,
+  useProgramMetadata,
+  useNFTs,
+  useSDK,
+} from "@thirdweb-dev/react/solana";
 
 // Default styles that can be overridden by your app
 require("@solana/wallet-adapter-react-ui/styles.css");
 
+const PROGRAM_ADDRESS = "6gQTNGV2K32yFAJuq4ya1BmTJFAcnaZxMztTrz6nXixn";
+
 const Home: NextPage = () => {
   // Here's how to get the thirdweb SDK instance
-  // const sdk = useSDK();
-  // Here's how to get a nft collection
-  // const { program } = useProgram(
-  //   your_nft_collection_address,
-  //   "nft-collection"
-  // );
+  const sdk = useSDK();
+  // Get the nft collection
+  const { program } = useProgram(
+    "6gQTNGV2K32yFAJuq4ya1BmTJFAcnaZxMztTrz6nXixn",
+    "nft-collection"
+  );
 
   return (
     <>
       <div className={styles.container}>
         <div className={styles.iconContainer}>
           <Image
-            src="/thirdweb.svg"
-            height={75}
-            width={115}
+            src="/cutemint.svg"
+            height={60}
+            width={100}
             objectFit="contain"
-            alt="thirdweb"
+            alt="cutemint"
           />
-          <Image
-            width={75}
-            height={75}
-            src="/sol.png"
-            className={styles.icon}
-            alt="sol"
-          />
+          <h1 className={styles.h1}>cutemint</h1>
         </div>
-        <h1 className={styles.h1}>Solana, meet thirdweb 👋</h1>
-        <p className={styles.explain}>
-          Explore what you can do with thirdweb&rsquo;s brand new{" "}
-          <b>
-            <a
-              href="https://portal.thirdweb.com/solana"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.lightPurple}
-            >
-              Solana SDK
-            </a>
-          </b>
-          .
-        </p>
 
         <WalletMultiButton />
       </div>
