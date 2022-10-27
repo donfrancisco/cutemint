8f258f4089884d25bb6e0826902341848bab82a5
Frank <frankinbox@gmail.com>
2022-10-26 19:21:41 -0500

    styling home module

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import {
   useNFTs,
   useSDK,
 } from "@thirdweb-dev/react/solana";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 // Default styles that can be overridden by your app
 require("@solana/wallet-adapter-react-ui/styles.css");
@@ -18,10 +19,13 @@ const Home: NextPage = () => {
   // Here's how to get the thirdweb SDK instance
   const sdk = useSDK();
   // Get the nft collection
-  const { program } = useProgram(
-    "6gQTNGV2K32yFAJuq4ya1BmTJFAcnaZxMztTrz6nXixn",
-    "nft-collection"
-  );
+
+  const { data: program } = useProgram(PROGRAM_ADDRESS, "nft-collection");
+  const { data: metadata, isLoading: loadingMetadata } =
+    useProgramMetadata(program);
+  const { data: nfts, isLoading } = useNFTs(program);
+
+  const { publicKey } = useWallet();
 
   return (
     <>
@@ -35,6 +39,22 @@ const Home: NextPage = () => {
             alt="cutemint"
           />
           <h1 className={styles.h1}>cutemint</h1>
+          {loadingMetadata ? (
+            <div className={styles.loading}>Loading...</div>
+          ) : (
+            <>
+              <h1 className={styles.h1}>{metadata?.name}</h1>
+              <div className={styles.iconContainer}>
+                <img
+                  className={styles.thumbnail}
+                  src={String(metadata?.image)}
+                  alt={String(metadata?.name)}
+                  height={120}
+                />
+              </div>
+              <p className={styles.explain}>{metadata?.description}</p>
+            </>
+          )}
         </div>
 
         <WalletMultiButton />
